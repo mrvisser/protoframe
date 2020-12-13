20df3558569c6a091c8062600d3d4f3b047461ac
Branden Visser <mrvisser@gmail.com>
2020-12-12 21:19:03 -0500

    improve performance of tests

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = (config) => {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/@babel/polyfill/dist/polyfill.js',
+      'node_modules/regenerator-runtime/runtime.js',
       'src/*.ts',
       'src/**/*.ts',
       'test/*.ts',
